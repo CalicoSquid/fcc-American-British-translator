2e02a9a9749cb3a9b8e6d04d43c39e6563645a8b
Caleb Mow <immensedrama@gmail.com>
2023-04-29 16:21:05 +0200

    Initial commit

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,125 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    // American to British translator
+    translateToBritish(text) {
+        
+        text = text.split(' ')
+        // Maps through array of input words
+        text
+        .map((item, index) => {
+            let spelling = americanToBritishSpelling[item]
+            let word = americanOnly[item]
+            let titles = americanToBritishTitles[item.toLowerCase()]
+            let twoWordArray = []
+            let keys = Object.keys(americanOnly)
+            // Sets an array of any phrases that have more that one word in them and pushes them to an array as a string
+            let moreThanOne = keys
+            .filter(key => key.indexOf(' ') >= 0)
+            .map(item => twoWordArray.push(item.split(' ')))
 
+            if (!isNaN(parseInt(item))) {
+                if (item.includes(':')) {
+                    let replaceTime = item.replace(':','.')
+                    text.splice(index, 1, '<span class="highlight">' + replaceTime + '</span>')
+                }   
+            }
+
+            twoWordArray.map((data, i) => {
+                if (data[0] === item) {
+                    let wordIndex = moreThanOne[i]
+                    let textIndex = text.indexOf(data[0])
+
+                    if (americanOnly[wordIndex] != undefined) {
+                        let check = text
+                        .slice(textIndex, textIndex + 2)
+                        .join(' ');
+
+                        if (check === data.join(' ')) {
+                            text.splice(index, 2, '<span class="highlight">' + americanOnly[wordIndex] + '</span>')
+                        } 
+                    }
+                }
+            })
+
+            if (word != undefined) {
+                text.splice(index, 1, '<span class="highlight">' + word + '</span>')
+            }
+            if (spelling != undefined) {
+                text.splice(index, 1, '<span class="highlight">' + spelling + '</span>')
+            }
+            if (titles != undefined) {
+                let x = titles.split('');
+                x[0] = x[0].toUpperCase()
+                text.splice(index, 1, '<span class="highlight">' + x.join('') + '</span>')
+            }
+        })
+
+        return text.join(' ')
+    }
+
+    translateToAmerican(text) {
+        
+        text = text.split(' ')
+        text
+        .map((item, index) => {
+            let word = britishOnly[item]
+
+            let titles = Object
+            .keys(americanToBritishTitles)
+            .find(key => americanToBritishTitles[key] === item.toLowerCase());
+            let spelling = Object
+            .keys(americanToBritishSpelling)
+            .find(key => americanToBritishSpelling[key] === item);
+            
+            let twoWordArray = []
+            let keys = Object.keys(britishOnly)
+            // Sets an array of any phrases that have more that one word in them and pushes them to an array as a string
+            let moreThanOne = keys
+            .filter(key => key.indexOf(' ') >= 0)
+            .map(item => twoWordArray.push(item.split(' ')))
+
+            if (!isNaN(parseInt(item))) {
+                if (item.includes('.')) {
+                    let replaceTime = item.replace('.',':')
+                    text.splice(index, 1, '<span class="highlight">' + replaceTime + '</span>')
+                }   
+            }
+
+            twoWordArray.map((data, i) => {
+                if (data[0] === item) {
+                    let wordIndex = moreThanOne[i]
+                    let textIndex = text.indexOf(data[0])
+
+                    if (britishOnly[wordIndex] != undefined) {
+                        let check = text
+                        .slice(textIndex, textIndex + 2)
+                        .join(' ');
+
+                        if (check === data.join(' ')) {
+                            text.splice(index, 2, '<span class="highlight">' + britishOnly[wordIndex] + '</span>')
+                        } 
+                    }
+                }
+            })
+
+            if (word != undefined) {
+                text.splice(index, 1, '<span class="highlight">' + word + '</span>')
+            }
+           
+            if(spelling != undefined) {
+                text.splice(index, 1, '<span class="highlight">' + spelling + '</span>')
+            }     
+
+            if(titles != undefined) {
+                let x = titles.split('');
+                x[0] = x[0].toUpperCase()
+                text.splice(index, 1, '<span class="highlight">' + x.join('') + '</span>')
+            }
+        })
+
+        return text.join(' ')
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,30 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let locale = req.body.locale;
+      let text = req.body.text;
+      let translated;
+      // If no text is entered
+      if (text === '') {
+        return res.json({ error: 'No text to translate' })
+      }
+      // If incorrect locale is entered
+      if ( locale !== 'american-to-british' && locale !== 'british-to-american') {
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+      // If one or more field is missing
+     if (!text || !locale) {
+       return res.json({ error: 'Required field(s) missing' })
+     }
+      // Call function depending on entered locale
+      locale === 'american-to-british' ?
+      translated = translator.translateToBritish(text) :
+      translated = translator.translateToAmerican(text);
+     // If no translation is needed (If returned value is equal to entered value)
+      if (translated === text) {
+        translated = 'Everything looks good to me!'
+      }
+      // Return json object
+      return res.json({text: text, translation: translated})
     });
 };
